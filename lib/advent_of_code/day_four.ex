d6f90660585240da702d0ff1525fa2cf79109db0
Paulo da Silva <paulo_silvalearn@hotmail.com>
2021-01-13 00:03:43 -0300

    [WIP] Add day four part two resolution.

--- lib/advent_of_code/day_four.ex
+++ lib/advent_of_code/day_four.ex
@@ -17,7 +17,7 @@ defmodule AdventOfCode.DayFour do
     File.cwd!()
     |> Kernel.<>("/lib/advent_of_code/inputs/day_four.input")
     |> File.read!()
-    |> String.split("\n\n")
+    |> String.split("\n\n", trim: true)
     |> Enum.map(&String.replace(&1, ["\n", " "], ";"))
   end
 
@@ -28,11 +28,16 @@ defmodule AdventOfCode.DayFour do
     |> Enum.count()
   end
 
-  def valid_passport?(passport) do
-    (@required_fields -- Map.keys(passport)) in [[], @optional_fields]
+  defp valid_passport?(passport) do
+    case @required_fields -- Map.keys(passport) do
+      rem when rem in [[], @optional_fields] ->
+        Enum.all?(passport, fn {field, value} -> validation(field, value) end)
+      _ ->
+        false
+    end
   end
 
-  def extract_data_from_document(document) do
+  defp extract_data_from_document(document) do
     document
     |> String.split(";")
     |> Enum.reduce(%{}, fn data, acc ->
@@ -41,5 +46,28 @@ defmodule AdventOfCode.DayFour do
         _ -> acc
       end
     end)
+  end
+
+  # (Country ID - optional)
+  defp validation("cid", _value), do: true
+
+  defp validation(field, value) do
+    validations = %{
+      "byr" => &(String.to_integer(&1) in 1920..2002), # (Birth Year)
+      "iyr" => &(String.to_integer(&1) in 2010..2020), # (Issue Year)
+      "eyr" => &(String.to_integer(&1) in 2020..2030), # (Expiration Year)
+      "hgt" => fn (height) ->
+        case Integer.parse(height) do
+          {h, "cm"} -> h in 150..193
+          {h, "in"} -> h in 59..76
+          _ -> false
+        end
+      end, # (Height)
+      "hcl" => &(Regex.match?(~r/#[0-9|a-f]{6}/, &1)), # (Hair Color)
+      "ecl" => &(&1 in ~w[amb blu brn gry grn hzl oth]), # (Eye Color)
+      "pid" => &(Regex.match?(~r/[0-9]{9}/, &1)), # (Passport ID)
+    }
+
+    validations[field].(value)
   end
 end
